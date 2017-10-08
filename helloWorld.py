72b2c54ac10ca193c65472788748d94d3cb7a77c
Shashank Addagarla <Shashank Addagarla>
2017-10-07 17:52:41 -0700

    oy vey

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,28 @@
+# set GOOGLE_APPLICATION_CREDENTIALS="D:/Coding/Whatever-21592b267981.json"
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Instantiates a client
+	client = language.LanguageServiceClient()
+
+	# The text to analyze
+	text = u'Hello, world!'
+	document = types.Document(
+	    content=text,
+	    type=enums.Document.Type.PLAIN_TEXT)
+
+	# Detects the sentiment of the text
+	sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+	print('Text: {}'.format(text))
+	return('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
 
 @app.route('/api/number')
